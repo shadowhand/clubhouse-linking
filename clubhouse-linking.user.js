07d5f3ed1b04f80436ab73d74821a0fab5400c82
Matt Scully <matt.scully@quarterspot.com>
2019-05-23 10:31:01 -0500

    Evaluate commit title and description

--- clubhouse-linking.user.js
+++ clubhouse-linking.user.js
@@ -55,11 +55,7 @@
             return; // Only operate on PRs and commits (for now)
         }
 
-        if (is_pull) {
-            const context = d.querySelector('.comment-body');
-        } else {
-            const context = d.querySelector('.commit-desc');
-        }
+        const context = is_pull ? d.querySelector('.comment-body') : d.querySelector('.full-commit');
 
         const nodes = query('.//text()[contains(., "[ch")]', context);
         nodes.forEach(update);
